aec4e3b0bfc22a781116136bd817e123357271c8
Vinicius <vini1718a@gmail.com>
2023-06-30 21:28:35 -0300

    ncompletion code

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -42,9 +42,49 @@ class NotesController {
      async delete(request, response) {
         const { id } = request.params;
 
-        await knex("notes").where({ id }).onDelete();
+        await knex("notes").where({ id }).delete();
 
         return response.json();
+     }
+
+     async index(request, response) {
+        const { title, user_id, movie_tags } = request.query;
+
+        let notes;
+
+        if(movie_tags) {
+            const filterTags = movie_tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("movie_tags")
+              .select(["notes.id", "notes.title", "notes.user_id"])
+
+              .where("notes.user_id", user_id)
+              .whereLike("notes.title", `%${title}%`)
+              .whereIn("name", filterTags)
+              .innerJoin("notes", "notes.id", "tags.note_id" )
+              .orderBy("notes.title")
+
+        }else {
+          notes = await knex("notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");
+        }
+
+        const userTags = await knex("movie_tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        });
+
+
+
+        return response.json(notesWithTags);
      }
 }
 
